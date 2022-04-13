5975d1ce7e65736dbb985b37a6137196fda17713
BryanRJR <bryanrantung98@gmail.com>
2022-04-13 18:20:01 +0700

    init menu scroll

--- app/src/main/java/com/hafidh/ujian_design/fragments/HomeFragment.kt
+++ app/src/main/java/com/hafidh/ujian_design/fragments/HomeFragment.kt
@@ -3,14 +3,19 @@ package com.hafidh.ujian_design.fragments
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
+import com.hafidh.ujian_design.R
 import com.hafidh.ujian_design.databinding.FragmentHomeBinding
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(){
     override val bindingInflater: (LayoutInflater) -> ViewBinding
         get() = FragmentHomeBinding::inflate
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         // todo disini
+
     }
 }
