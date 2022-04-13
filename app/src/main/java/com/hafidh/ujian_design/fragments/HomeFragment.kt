c0f15388ba8d31aa2fad4ce2ab1f014e6b84b0b3
Mufti Ramdhani <muftiramdhani25@gmail.com>
2022-04-13 15:20:02 +0300

    slicing app bar and toolbar

--- app/src/main/java/com/hafidh/ujian_design/fragments/HomeFragment.kt
+++ app/src/main/java/com/hafidh/ujian_design/fragments/HomeFragment.kt
@@ -1,9 +1,9 @@
 package com.hafidh.ujian_design.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
+import android.view.*
 import androidx.viewbinding.ViewBinding
+import com.hafidh.ujian_design.R
 import com.hafidh.ujian_design.databinding.FragmentHomeBinding
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(){
@@ -12,5 +12,6 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         // todo disini
+        binding.toolbar.inflateMenu(R.menu.menu_appbar)
     }
 }
