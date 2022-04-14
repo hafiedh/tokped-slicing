66b3df8de3c2610cb8d2648e0c3b7e034c3b05cc
hafiedh <muhhafiedh@gmail.com>
2022-04-14 19:08:01 +0700

    fix force closed bottom navigation

--- app/src/main/java/com/hafidh/ujian_design/fragments/BaseFragment.kt
+++ app/src/main/java/com/hafidh/ujian_design/fragments/BaseFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseFragment<out T: ViewBinding>: Fragment() {
+abstract class BaseFragment<out T: ViewBinding?>: Fragment() {
     private var _binding: ViewBinding? = null
 
     @Suppress("UNCHECKED_CAST")
@@ -22,10 +22,6 @@ abstract class BaseFragment<out T: ViewBinding>: Fragment() {
         return _binding!!.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 
     protected abstract val bindingInflater: (LayoutInflater) -> ViewBinding
 
